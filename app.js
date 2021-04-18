f5046b144147a65ac8b4004c2aa95db42a64f9f9
michael oreilly <michaelheartbound99@hotmail.com>
2021-04-18 14:46:42 -0600

    4th commit add department

--- app.js
+++ app.js
@@ -65,3 +65,85 @@ var connection = mysql.createConnection({
         }
       });
   }
+
+  getRoles = () => {
+    connection.query("SELECT id, title FROM role", (err, res) => {
+      if (err) throw err;
+      roles = res;
+      // console.table(roles);
+    })
+  };
+  
+  getDepartments = () => {
+    connection.query("SELECT id, name FROM department", (err, res) => {
+      if (err) throw err;
+      departments = res;
+      // console.log(departments);
+    })
+  };
+  
+  getManagers = () => {
+    connection.query("SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee", (err, res) => {
+      if (err) throw err;
+      managers = res;
+      // console.table(managers);
+    })
+  };
+  
+  getEmployees = () => {
+    connection.query("SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee", (err, res) => {
+      if (err) throw err;
+      employees = res;
+      // console.table(employees);
+    })
+  };
+  
+  addSomething = () => {
+    inquirer.prompt([
+      {
+        name: "add",
+        type: "list",
+        message: "What would you like to add?",
+        choices: ["DEPARTMENT", "ROLE", "EMPLOYEE", "EXIT"]
+      }
+    ]).then(function(answer) {
+      if (answer.add === "DEPARTMENT") {
+        console.log("Add a new: " + answer.add);
+        addDepartment();
+      }
+      else if (answer.add === "ROLE") {
+        console.log("Add a new: " + answer.add);
+        addRole();
+      }
+      else if (answer.add === "EMPLOYEE") {
+        console.log("Add a new: " + answer.add);
+        addEmployee();
+      } 
+      else if (answer.add === "EXIT") {
+        figlet('Thanks for using FSC Employee Tracker', (err, result) => {
+          console.log(err || result);
+        });
+  
+        connection.end();
+      } else {
+        connection.end();
+      }
+    })
+  };
+  
+  addDepartment = () => {
+    inquirer.prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "What department would you like to add?"
+      }
+    ]).then(function(answer) {
+      connection.query(`INSERT INTO department (name) VALUES ('${answer.department}')`, (err, res) => {
+        if (err) throw err;
+        console.log("1 new department added: " + answer.department);
+        getDepartments();
+        start();
+      }) 
+    })
+  };
